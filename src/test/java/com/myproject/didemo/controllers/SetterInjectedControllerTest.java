66e589ce692f10d9d343c7dad23802e25dc6fc45
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-10-29 13:21:45 +0100

    add Tests

---
+++ src/test/java/com/myproject/didemo/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package com.myproject.didemo.controllers;
+
+import com.myproject.didemo.controllers.SetterInjectedController;
+import com.myproject.didemo.services.GreetingServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SetterInjectedControllerTest {
+
+    private SetterInjectedController setterInjectedController;
+
+    @Before
+    public void setUp() throws Exception {
+        this.setterInjectedController = new SetterInjectedController();
+        this.setterInjectedController.setGreetingService((new GreetingServiceImpl()));
+    }
+
+    @Test
+    public void testGreeting() throws Exception {
+        assertEquals(GreetingServiceImpl.HELLO_GURUS, setterInjectedController.sayHello());
+    }
+}
